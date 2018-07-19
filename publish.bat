2422bfa281b6c8e9fb796a797a923784b8e4c98b
Derek K Schmid <derek@srsolutions.biz>
2018-07-18 21:16:44 -0400

    Trigger rebuild

--- publish.bat
+++ publish.bat
@@ -3,6 +3,5 @@ SET distPath=dist\personal-site
 
 xcopy /s/y %distPath% %projectPath%
 
-REM git --git-dir "%projectPath%.git" add %projectPath%
 git --git-dir "%projectPath%.git" commit -am "Trigger rebuild"
 git --git-dir "%projectPath%.git" push

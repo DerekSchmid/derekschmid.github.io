44f6e01995105607a29de20ca8269810820cf489
Derek K Schmid <derek@srsolutions.biz>
2018-07-18 20:45:03 -0400

    Trigger rebuild

--- publish.bat
+++ publish.bat
@@ -1,7 +1,7 @@
 SET projectPath=..\derekschmid.github.io\
 SET distPath=dist\personal-site
 
-xcopy /s/y "dist\personal-site" %projectPath%
+xcopy /s/y %distPath% %projectPath%
 
 git --git-dir "%projectPath%.git" add .
 git --git-dir "%projectPath%.git" commit -m "Trigger rebuild"
